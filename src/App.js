9fa044b51e06cba2e3bdee790e85ca9f7b4989eb
Sleather Vega <sleatherv28@hotmail.com>
2021-01-20 21:25:43 -0500

    Creating Form to add tasks and uuid function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import FormTasks from './components/FormTasks';
 
 const App = () => {
+  const [tasks, changeTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Wash the dishes',
+        completed: false
+      },
+      {
+        id: 2,
+        text: 'Go to the supermarket',
+        completed: false
+      },
+    ]
+  );
+  console.log(tasks);
   return (
-    <div><h1>Hello World</h1></div>
+    <div className='container'>
+      <Header />
+      <FormTasks tasks={tasks} changeTasks={changeTasks} />
+    </div>
   );
 }
 
---
+++ src/components/FormTasks.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { v4 as uuidv4 } from 'uuid';
+
+const FormTasks = ({ tasks, changeTasks }) => {
+    const [inputTask, changeInputTask] = useState('');
+
+    const handleInput = (e) => {
+        changeInputTask(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        changeTasks(
+            [
+                ...tasks,
+                {
+                    id: uuidv4(),
+                    text: inputTask,
+                    completed: false
+                }
+            ]
+        );
+    }
+    return (
+        <form action="" className='form-tasks' onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className='form-tasks__input'
+                placeholder='Write a task'
+                value={inputTask}
+                onChange={(e) => { handleInput(e) }}
+            />
+            <button
+                type='submit'
+                className='form-tasks__btn'
+            >
+                <FontAwesomeIcon
+                    icon={faPlusSquare} className='form-tasks__icono-btn'
+                />
+            </button>
+        </form>
+    );
+}
+
+
+export default FormTasks;
