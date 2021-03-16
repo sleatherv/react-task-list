16374de08d09064c36c68bfbe5b8e2f78be0ce70
Sleather Vega <sleatherv28@hotmail.com>
2021-03-15 22:35:10 -0500

    Saving Tasks status in local storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import FormTasks from './components/FormTasks';
 import TasksList from './components/TasksList';
 
 const App = () => {
-  const [tasks, changeTasks] = useState(
-    [
-      {
-        id: 1,
-        text: 'Wash the dishes',
-        completed: false
-      },
-      {
-        id: 2,
-        text: 'Go to the supermarket',
-        completed: true
-      },
-    ]
-  );
-  const [showCompleted, setShowCompleted] = useState(false);
+  //Get saved tasks from local storage
+  const savedTasks = localStorage.getItem('tasks')
+    ? JSON.parse(localStorage.getItem('tasks'))
+    : [];
+
+  //Setting the status of tasks
+  const [tasks, changeTasks] = useState(savedTasks);
+
+  //Saving the status in localstorage
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+
+  //Get saved showCompleted value from local storage
+  let savedshowCompleted = ''
+  if (localStorage.getItem('showCompleted') === null) {
+    savedshowCompleted = true;
+  } else {
+    savedshowCompleted = localStorage.getItem('showCompleted') === 'true'
+  }
+
+  //ShowCompleted status
+  const [showCompleted, setShowCompleted] = useState(savedshowCompleted);
+
+  //Saving the status of showcompleted in localstorage
+  useEffect(() => {
+    localStorage.setItem('showCompleted', showCompleted.toString());
+  }, [showCompleted]);
 
   return (
     <div className='container'>
